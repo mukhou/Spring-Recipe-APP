a18259b9dc824a85afb0b382372aad6769d9726e
jhini <banerjeeurmi@gmail.com>
2020-02-10 16:53:22 -0500

    Adding swagger

---
+++ src/main/java/guru/springframework/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+/* 
+User: Urmi
+Date: 2/10/2020 
+Time: 4:09 PM
+*/
+
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
